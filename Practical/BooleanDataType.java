ccbe69f04dfaba556585ee9cc3793630cf3d8cc8
KajalMagar8 <105428097+KajalMagar8@users.noreply.github.com>
2022-05-12 16:33:51 +0530

    Add files via upload

---
+++ Practical/BooleanDataType.java
@@ -0,0 +1,16 @@
+package DataTypes;
+
+public class BooleanDataType 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Program for Boolean Datatype:\n");
+		boolean myBool = true;
+		if(myBool == true)
+		{
+			System.out.println("I am using Boolean data type:"+myBool);
+		}
+	}
+
+}
---
+++ Practical/FloatDataType.java
@@ -0,0 +1,19 @@
+package DataTypes;
+
+public class FloatDataType 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Program for Float Datatype:");
+		
+		float myFloat1,myFloat2,result3;
+		myFloat1 = 1000.666f;
+		myFloat2 = 110.77f;
+		result3 = myFloat1 - myFloat2;
+		System.out.println("Number1:"+myFloat1);
+		System.out.println("Number2:"+myFloat2);
+		System.out.println("Number1 - Number2 :"+result3);
+	}
+
+}
