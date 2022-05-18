f0eb22d6b58e7068f46cfbf815f3789e63cce28b
KajalMagar8 <105428097+KajalMagar8@users.noreply.github.com>
2022-05-18 15:55:34 +0530

    Add files via upload

---
+++ Practical/18May2022/RelationalOperator.java
@@ -0,0 +1,19 @@
+package Operators;
+
+public class RelationalOperator
+{
+
+	public static void main(String[] args) 
+	{
+		
+		  int num1 = 12, num2 = 4;
+		 
+		  System.out.println("num1 == num2 = " + (num1 == num2) );
+		  System.out.println("num1 != num2 = " + (num1 != num2) );
+		  System.out.println("num1 >  num2 = " + (num1 >  num2) );		 
+		  System.out.println("num1 <  num2 = " + (num1 <  num2) );
+		  System.out.println("num1 >= num2 = " + (num1 >= num2) );
+		  System.out.println("num1 <= num2 = " + (num1 <= num2) );
+	}
+
+}
